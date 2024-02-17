3f8eaed82d4957df44565457b7d2f3237547f0d1
Bryan Su <bsuia00@gmail.com>
2024-02-16 18:31:59 -0600

    Hello World Setup

---
+++ 2D-Implementation/2D-Implementation/Program.cs
@@ -0,0 +1,21 @@
+﻿using Raylib_cs;
+
+class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "2D Surface Destruction Testing");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Color.White);
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
