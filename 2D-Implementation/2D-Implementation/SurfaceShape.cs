e992f1f2297747257a6cb0992076dc7123b85261
Bryan Su <bsuia00@gmail.com>
2024-02-23 17:43:48 -0600

    Reworked surfaceshape

---
+++ 2D-Implementation/2D-Implementation/SurfaceShape.cs
@@ -0,0 +1,29 @@
+﻿
+namespace PSDSystem
+{
+    public class SurfaceShape
+    {
+        public List<PolygonPair> Polygons { get; }
+
+        public SurfaceShape()
+        {
+            Polygons = new List<PolygonPair>();
+        }
+    }
+
+    public class PolygonPair
+    {
+        public Polygon<PolygonVertex> OuterPolygon { get; }
+
+        public List<Polygon<PolygonVertex>> InnerPolygons { get; }
+
+        public PolygonPair(Polygon<PolygonVertex>? outerPolygon, List<Polygon<PolygonVertex>>? innerPolygons = null)
+        {
+            if (outerPolygon != null) OuterPolygon = outerPolygon;
+            else OuterPolygon = new Polygon<PolygonVertex>();
+
+            if (innerPolygons != null) InnerPolygons = innerPolygons;
+            else InnerPolygons = new List<Polygon<PolygonVertex>>();
+        }
+    }
+}
