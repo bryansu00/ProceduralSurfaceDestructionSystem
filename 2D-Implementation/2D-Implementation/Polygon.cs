8b95d49e67c9c6ba6bc3620c14aeb50a44479a5f
Bryan Su <bsuia00@gmail.com>
2024-02-22 19:58:39 -0600

    Working on Polygon

--- 2D-Implementation/2D-Implementation/Polygon.cs
+++ 2D-Implementation/2D-Implementation/Polygon.cs
@@ -1,11 +1,11 @@
-﻿
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
 
 namespace PSDSystem
 {
-    public class Polygon
+    public class Polygon<T> where T : PolygonVertex
     {
-        public PolygonVertex? HeadVertex { get; private set; }
+        public VertexNode<T>? Head { get; private set; }
 
         public uint Size { get; private set; }
 
@@ -13,9 +13,85 @@ namespace PSDSystem
 
         public Polygon()
         {
-            HeadVertex = null;
+            Head = null;
             Size = 0;
             Vertices = null;
         }
+
+        public VertexNode<T> InsertVertexAtBack(int center)
+        {
+            T? data = (T?)Activator.CreateInstance(typeof(T), center);
+
+            if (data == null) throw new Exception();
+
+            VertexNode<T> newNode = new VertexNode<T>(this, data);
+
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                // Get tail node
+                VertexNode<T> tail = Head.Previous;
+
+                // Connect tail node to new node
+                tail.Next = newNode;
+                newNode.Previous = tail;
+
+                // Connect new node to Head
+                Head.Previous = newNode;
+                newNode.Next = Head;
+            }
+
+            // Increment size of the polygon
+            Size++;
+
+            return newNode;
+        }
+
+        public List<int> ToList()
+        {
+            List<int> toReturn = new List<int>();
+            if (Head == null) return toReturn;
+
+            var now = Head;
+            do
+            {
+                toReturn.Add(now.Data.Center);
+                now = now.Next;
+            } while (now != Head);
+
+            return toReturn;
+        }
     }
+
+    public class PolygonVertex
+    {
+        public int Center { get; }
+
+        public PolygonVertex(int center)
+        {
+            Center = center;
+        }
+    }
+
+    public class VertexNode<T> where T : PolygonVertex
+    {
+        public T Data { get; }
+
+        public VertexNode<T> Next { get; set; }
+
+        public VertexNode<T> Previous { get; set; }
+
+        public Polygon<T> Owner { get; }
+
+        public VertexNode(Polygon<T> owner, T data)
+        {
+            Owner = owner;
+            Next = this;
+            Previous = this;
+            Data = data;
+        }
+}
 }
--- 2D-Implementation/2D-Implementation/Program.cs
+++ 2D-Implementation/2D-Implementation/Program.cs
@@ -9,6 +9,23 @@ class Program
         Raylib.InitWindow(800, 480, "2D Surface Destruction Testing");
         Raylib.SetTargetFPS(60);
 
+        // Initialize stuff....
+
+        List<Vector2> Vertices = new List<Vector2>();
+        Vertices.Add(new Vector2(1050.0f, 150.0f));
+        Vertices.Add(new Vector2(150.0f, 150.0f));
+        Vertices.Add(new Vector2(150.0f, 550.0f));
+        Vertices.Add(new Vector2(1050.0f, 550.0f));
+
+        Polygon<PolygonVertex> polygon = new Polygon<PolygonVertex>();
+        polygon.Vertices = Vertices;
+        polygon.InsertVertexAtBack(0);
+        polygon.InsertVertexAtBack(1);
+        polygon.InsertVertexAtBack(2);
+        polygon.InsertVertexAtBack(3);
+
+        Console.WriteLine(String.Join(", ", polygon.ToList()));
+
         while (!Raylib.WindowShouldClose())
         {
             Raylib.BeginDrawing();
